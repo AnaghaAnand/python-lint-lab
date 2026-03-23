680038ed3f8130f948f1ef458b60ecbc5a5596b4
Anagha Anand <anaghaa0511@gmail.com>
2026-03-23 17:57:40 +0530

    Introduce lint error

--- calculator.py
+++ calculator.py
@@ -1,11 +1,4 @@
-"""Simple calculator module"""
+def add(a, b): return a + b
 
-
-def add(number_one, number_two):
-    """Return the sum of two numbers."""
-    return number_one + number_two
-
-
-def subtract(number_one, number_two):
-    """Return the difference of two numbers."""
-    return number_one - number_two
+def subtract(a, b):
+    return a - b
